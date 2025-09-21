8188e0cce663704996cad2ff832e97d889253992
alinagalstyan30-rgb <alinagalstyan30@gmail.com>
2025-09-21 18:32:02 +0400

    Alinahomework

---
+++ script.js
@@ -0,0 +1,64 @@
+1
+// let num = prompt("write a number")
+// let result;
+
+// if(num > 0){
+//   result=1;
+// } else if(num < 0){
+//    result=-1;
+// } else {
+//    result=0
+// }
+
+//alert(result);
+
+2
+// let result;
+
+// if (a + b < 4) {
+//   result = 'malo';
+// } else {
+//    result = 'mnogo';
+// }
+
+// let result=(a + b < 4) ? 'malo': 'mnogo';
+
+3
+// for(let i=0; i<=40; i+=2){
+//    console.log(i);
+// }
+
+4
+// let number=1;
+// for(let i=1; i<=30; i++){
+//   number*=i;
+// }
+// console.log(number);
+
+5
+// let result=0;
+// let i=1;
+// while(i<=40){
+//   if(i%2==0){
+//     result+=i;
+//  }
+//  i++
+// }
+// console.log(result);
+
+6
+// let i=1
+// while(i<=40){
+//   if(i%7===0){
+//       console.log(i);
+//   }
+//    i++
+// }
+
+7
+let number=1;
+for(let i=1; i<=60; i++){
+    if(i%6==0){
+       number*=i; 
+    }
+} console.log(number); 
